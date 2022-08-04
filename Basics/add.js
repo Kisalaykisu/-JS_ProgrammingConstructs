146ea250ce9bc90e82034d4abdf34bc223dfefb6
Kisalay <kisalaykisu@gmail.com>
2022-08-05 00:59:06 +0530

    file has been added

--- Basics/add.js
+++ Basics/add.js
@@ -1,3 +1,5 @@
+// based on the current condition
+
 let x = 5;         // assign the value 5 to x
 let y = 2;         // assign the value 2 to y
 let z = x + y;     // assign the value 7 to z (5 + 2)
